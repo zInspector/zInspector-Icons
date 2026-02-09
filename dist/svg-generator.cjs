b061316bc837b8b4deb5f0480209a71619f4d4f5
JoelzInspector <39933403+JoelzInspector@users.noreply.github.com>
2026-02-09 15:16:58 -0300

    Update ZCarpetCleaningIcon SVG paths

--- dist/svg-generator.cjs
+++ dist/svg-generator.cjs
@@ -101,9 +101,7 @@ var ZCarpetCleaningIcon = ({
             cy: "175.843",
             r: "42.7807",
             stroke: color,
-            strokeWidth,
-            strokeLinecap: "round",
-            strokeLinejoin: "round"
+            strokeWidth
           }
         ),
         /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
@@ -111,9 +109,7 @@ var ZCarpetCleaningIcon = ({
           {
             d: "M31.105 142V104.422C31.105 103.87 31.5527 103.422 32.105 103.422H121.631C145.677 103.422 193.674 124.978 194.05 211.033C194.053 211.585 193.605 212.053 193.052 212.053H89",
             stroke: color,
-            strokeWidth,
-            strokeLinecap: "round",
-            strokeLinejoin: "round"
+            strokeWidth
           }
         ),
         /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
@@ -122,8 +118,7 @@ var ZCarpetCleaningIcon = ({
             d: "M157.842 115.491V33C157.842 31.8954 158.737 31 159.842 31H228.263C229.367 31 230.263 31.8954 230.263 33V212.053M230.263 212.053H218.193M230.263 212.053H242.333",
             stroke: color,
             strokeWidth,
-            strokeLinecap: "round",
-            strokeLinejoin: "round"
+            strokeLinecap: "round"
           }
         )
       ]
--- src/react-web-only.tsx
+++ src/react-web-only.tsx
@@ -227,9 +227,9 @@ const ZCarpetCleaningIconWeb: React.FC<IconProps> = ({
 
   return (
     <svg width={size} height={size} viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="61.2807" cy="175.843" r="42.7807" stroke={color} stroke-width={strokeWidth} />
-      <path d="M31.105 142V104.422C31.105 103.87 31.5527 103.422 32.105 103.422H121.631C145.677 103.422 193.674 124.978 194.05 211.033C194.053 211.585 193.605 212.053 193.052 212.053H89" stroke={color} stroke-width={strokeWidth} />
-      <path d="M157.842 115.491V33C157.842 31.8954 158.737 31 159.842 31H228.263C229.367 31 230.263 31.8954 230.263 33V212.053M230.263 212.053H218.193M230.263 212.053H242.333" stroke={color} stroke-width={strokeWidth} />
+      <circle cx="61.2807" cy="175.843" r="42.7807" stroke={color} strokeWidth={strokeWidth} />
+      <path d="M31.105 142V104.422C31.105 103.87 31.5527 103.422 32.105 103.422H121.631C145.677 103.422 193.674 124.978 194.05 211.033C194.053 211.585 193.605 212.053 193.052 212.053H89" stroke={color} strokeWidth={strokeWidth} />
+      <path d="M157.842 115.491V33C157.842 31.8954 158.737 31 159.842 31H228.263C229.367 31 230.263 31.8954 230.263 33V212.053M230.263 212.053H218.193M230.263 212.053H242.333" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" />
     </svg>
   );
 
